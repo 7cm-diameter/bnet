a5c292806f02561af8af8608649f38480de9955b
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-08-10 14:17:26 +0900

    Remove `_` from the head of some functions to allow calls from different modules

--- bnet/__init__.py
+++ bnet/__init__.py
@@ -5,13 +5,13 @@ import scipy.stats as st
 from numpy.typing import NDArray
 
 
-def _softmax(x: NDArray[np.float_], beta: float) -> NDArray[np.float_]:
+def softmax(x: NDArray[np.float_], beta: float) -> NDArray[np.float_]:
     xmax = np.max(x)
     x_ = np.exp((x - xmax) * beta)
     return x_ / np.sum(x_)
 
 
-def _propotional_allocation(x: NDArray[np.float_]) -> NDArray[np.float_]:
+def propotional_allocation(x: NDArray[np.float_]) -> NDArray[np.float_]:
     probs = x / np.sum(x)
     return probs / np.sum(probs)
 
