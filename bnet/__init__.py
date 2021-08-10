263899793b0b11861c080d41f6c87a0bea5a346a
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-08-10 11:41:35 +0900

    Add functions that generate random number

--- bnet/__init__.py
+++ bnet/__init__.py
@@ -1,6 +1,7 @@
 __version__ = '0.1.0'
 
 import numpy as np
+import scipy.stats as st
 from numpy.typing import NDArray
 
 
@@ -13,3 +14,11 @@ def _softmax(x: NDArray[np.float_], beta: float) -> NDArray[np.float_]:
 def _propotional_allocation(x: NDArray[np.float_]) -> NDArray[np.float_]:
     probs = x / np.sum(x)
     return probs / np.sum(probs)
+
+
+def exp_rng(mean: float, n: int, _min: float) -> NDArray[np.float_]:
+    return st.expon.ppf(np.linspace(0.001, 0.999, n), scale=mean, loc=_min)
+
+
+def geom_rng(mean: float, n: int, _min: float) -> NDArray[np.float_]:
+    return st.geom.ppf(np.linspace(0.001, 0.999, n), p=1 / mean, loc=_min)
