20299417561b8d82e5782771d6d684f0be7b7afd
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-08-11 12:25:37 +0900

    Add a function to train an agent under a given environment

--- hbtfmt/__init__.py
+++ hbtfmt/__init__.py
@@ -2,6 +2,12 @@ import bnet.typing as tp
 import numpy as np
 from numpy.typing import NDArray
 
+from hbtfmt.env import ConcurrentSchedule
+
+
+def as_onehot(x: int, n: int) -> NDArray[np.int_]:
+    return np.identity(n)[x]
+
 
 def free_access(agent: tp.Agent, rewards: NDArray[np.float_],
                 travel: int) -> float:
@@ -20,3 +26,21 @@ def free_access(agent: tp.Agent, rewards: NDArray[np.float_],
         number_of_overall += len(response_sequence)
         current_response = next_response
     return number_of_operant / number_of_overall
+
+
+# We assumed that an agent's behavior as a choice
+# between the operant response and other responses
+# so all enviroments used in simulations are conccurent schedules
+def train(agent: tp.Agent, env: ConcurrentSchedule):
+    n = agent.n
+    current_response = np.random.choice(n)
+    main_schedule_continue = not env.finished()[0]
+
+    while main_schedule_continue:
+        next_response = np.random.choice(n)
+        t = agent.engage_response(next_response)
+        onehot_response = as_onehot(next_response, n)
+        reward = env.step(onehot_response, t)
+        agent.update(current_response, next_response, reward)
+        current_response = next_response
+        main_schedule_continue = not env.finished()[0]
