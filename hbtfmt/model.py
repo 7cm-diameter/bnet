39e03440b7a20938dbc58b8228d0bfc9138186c5
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-09-24 12:56:56 +0900

    Add an agent to reproduce the bout and pause pattern

--- hbtfmt/model.py
+++ hbtfmt/model.py
@@ -3,6 +3,7 @@ import numpy as np
 import numpy.typing as npt
 from bnet import propotional_allocation
 from bnet.network import SimpleBehavioralNetwork
+from scipy.stats import poisson
 
 
 class HQAgent(tp.Agent):
@@ -65,6 +66,57 @@ class QAgent(tp.Agent):
     def choose_action(self, rewards: npt.NDArray[tp.Reward]) -> tp.Node:
         probs = propotional_allocation(rewards)
         return np.random.choice(self._n, p=probs)
+
+    def engage_response(self, response: tp.Node) -> tp.ResponseTime:
+        return self._response_times[response]
+
+    @property
+    def n(self) -> int:
+        return self._n
+
+    @property
+    def network(self) -> SimpleBehavioralNetwork:
+        return self._net
+
+
+class QAgentWithBout(tp.Agent):
+    def __init__(self, n: int, alpha: float,
+                 response_times: npt.NDArray[tp.ResponseTime],
+                 boutlenght: int):
+        self._n = n
+        self._alpha = alpha
+        self._boutlength = boutlenght
+        self._inbout = 0
+        self._q_values = np.full((n, n), 1e-6)
+        self._response_times = response_times
+        self._net = SimpleBehavioralNetwork()
+
+    def construct_network(self, min_: int):
+        self._net.construct_network(self._q_values, propotional_allocation,
+                                    min_)
+
+    def generate_action_sequence(self, s: tp.Node, t: tp.Node) -> tp.Path:
+        paths = self._net.find_path(s, t)
+        i = np.random.choice(len(paths))
+        path: list[tp.Node] = paths[i]
+        # if 0 in path:
+        #     operant_idx = path.index(0)
+        #     [path.insert(operant_idx, 0) for _ in range(boutlength)]
+        return path[1:]
+
+    def update(self, s: tp.Node, t: tp.Node, reward: tp.Reward):
+        self._q_values[s][t] += self._alpha * (reward - self._q_values[s][t])
+
+    def choose_action(self, rewards: npt.NDArray[tp.Reward]) -> tp.Node:
+        if self._inbout > 0:
+            self._inbout -= 1
+            return 0
+        probs = propotional_allocation(rewards)
+        response = np.random.choice(self._n, p=probs)
+        if response == 0 and self._inbout == 0:
+            self._inbout = poisson.ppf(np.random.uniform(),
+                                       mu=self._boutlength).astype(np.int_)
+        return response
 
     def engage_response(self, response: tp.Node) -> tp.ResponseTime:
         return self._response_times[response]
